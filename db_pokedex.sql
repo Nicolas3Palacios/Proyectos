1a4b3fc1fd993edfec5843017a5199e9b6ab20a0
NikTrBr <104227596+NikTrBr@users.noreply.github.com>
2022-04-22 14:52:14 -0500

    Add files via upload

---
+++ db_pokedex.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-04-2022 a las 21:30:56
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_pokedex`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `correo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `pass` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ register.php
@@ -0,0 +1,49 @@
+<?php
+ 
+include('config.php');
+session_start();
+
+$query = 0;
+$result = '';
+ 
+if (isset($_POST['register'])) {
+ 
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+    $query = mysqli_query($conexion, "SELECT * FROM usuarios where correo = '$email'");
+    $result = mysqli_fetch_array($query);
+    if ($result > 0) {
+        
+         echo '<p class="error">El correo ya ha sido registrado!</p>';
+    } else {
+        $query_insert = mysqli_query($conexion, "INSERT INTO usuarios(correo,pass) values ('$email', '$password_hash')");
+        if ($query_insert) {
+            echo '<p class="success">Usuario registrado exitosamente!</p>';
+        } else {
+            echo '<p class="error">Error!</p>';
+        }
+    }
+    
+}    
+
+?>
+<link rel="stylesheet" href="./css/stylesLogin.css">
+<div class="container">
+
+</div>
+<form method="post" action="" name="signup-form">
+    <p class="titulo-ic">Registrar Usuario</p>
+    <div class="form-element">
+        <label>Correo</label>
+        <input type="email" name="email" required />
+    </div>
+    <div class="form-element">
+        <label>Contraseña</label>
+        <input type="password" name="password" required />
+    </div>
+    <button type="submit" name="register" value="register">Registrar</button>
+    <hr>
+    <a href="index.php">Volver</a>
+</form>
